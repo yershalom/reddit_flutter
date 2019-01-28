9b9f84e9bb00ce233ba738a4a45ef8d99fa0b24e
Shalom Yerushalmy <yershalom@gmail.com>
2019-01-28 15:54:53 +0200

    Forgot to add the Options

---
+++ lib/widgets/SelectedOption.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+
+class CustomPopupMenu {
+  CustomPopupMenu({this.title, this.icon});
+
+  String title;
+  IconData icon;
+}
+
+class SelectedOption extends StatelessWidget {
+  final CustomPopupMenu choice;
+
+  SelectedOption({Key key, this.choice}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Icon(choice.icon, size: 140.0, color: Colors.white),
+            Text(
+              choice.title,
+              style: TextStyle(color: Colors.white, fontSize: 30),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
